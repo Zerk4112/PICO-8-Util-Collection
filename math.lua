ee02be3cd2423843f345ee35339beff12ebb31ab
Zachariah Covington <zachcovi@gmail.com>
2023-08-26 19:09:25 -0500

    :pencil: style: Added Documentation Header
    - Added a standardized documentation header to the
        top of the file.
    
    <footer>

--- math.lua
+++ math.lua
@@ -1,3 +1,22 @@
+-- math.lua
+--
+-- A collection of math utility functions for PICO-8.
+--
+-- Functions:
+--   randib(a,b) --> integer
+--   aget(x1,y1,x2,y2) --> float
+--   dst(x0,y0,x1,y1) --> float
+--   sqr(x) --> float
+--
+-- Usage:
+--   #include "lib/math.lua"
+--   local x = randib(1, 10)
+--   local y = randb(1, 10)
+--   local d = dst(0, 0, 10, 10)
+--   local s = sqr(10)
+--   local a = aget(0, 0, 10, 10)
+
+
 function aget(x1,y1,x2,y2) return atan2(-(x1-x2), -(y1-y2)) end
 
 function dst(x0,y0,x1,y1)
